6deb337459888015c9e921ca82b8d33552bb04db
Ryan Griffith <ryan.griffith@hannonhill.com>
2012-08-24 18:23:49 -0400

    Updated the test read() method to use the Savon body hash feature instead of building the SOAP request manually.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,51 +5,16 @@ class MainController < ApplicationController
     # create a client for the Asset Operation SOAP service
     @opsClient = Savon.client("http://localhost:7030/ws/services/AssetOperationService?wsdl")    
     
+    # sample listMessages call
     @listMessagesResponse = @opsClient.request :list_messages do
-      soap.namespaces["xmlns:ass"] = "http://www.hannonhill.com/ws/ns/AssetOperationService"
       soap.body = { 
-        "ass:authentication" => { 
-          "ass:username" => "admin", 
-          "ass:password" => "admin" 
+        "authentication" => { 
+          "username" => "admin", 
+          "password" => "admin" 
         }
       }
-    end    
-    
-    # Example of building a SOAP request manually...    
-    @readResponse = @opsClient.request :read do
-      namespaces = {
-        "xmlns:soapenv" => "http://schemas.xmlsoap.org/soap/envelope/",
-        "xmlns:ops" => "http://www.hannonhill.com/ws/ns/AssetOperationService"
-      }
-      soap.xml do |xml|
-        xml.soapenv(:Envelope, namespaces) do |xml|
-          xml.soapenv(:Body) do |xml|
-            xml.ops(:read) do |xml|
-              xml.ops(:authentication) do |xml|         
-                xml.ops(:password, "admin")
-                xml.ops(:username, "admin")
-              end
-              xml.ops(:identifier) do |xml|         
-                xml.ops(:id, "b54d846f0a00016c6a59d8da787f87cf")
-                xml.ops(:type, "page")
-              end
-            end
-          end        
-        end 
-      end
-    end
-    
-    # create a client for the Security SOAP service
-    @securityClient = Savon.client("http://localhost:7030/ws/services/SecurityService?wsdl")        
-    @authenticateResponse = @securityClient.request :authenticate do
-      soap.body = { 
-        :authentication => { 
-          :username => "admin", 
-          :password => "admin" 
-        }
-      }
-    end
-    
+    end   
+            
     # Example of building a SOAP request manually...    
     #@listMessagesResponse = @opsClient.request :list_messages do
     #  namespaces = {
@@ -68,7 +33,34 @@ class MainController < ApplicationController
     #      end        
     #    end 
     #  end
-    #end
+    #end 
     
+    # sample read (Page) call
+    @readResponse = @opsClient.request :read do
+      soap.body = { 
+        "authentication" => {
+          "password" => "admin",
+          "username" => "admin"
+        },
+        "identifier" => { 
+          "id" => "b54d846f0a00016c6a59d8da787f87cf", 
+          "type" => "page"
+        }
+      }
+    end
+    
+    # create a client for the Security SOAP service
+    @securityClient = Savon.client("http://localhost:7030/ws/services/SecurityService?wsdl")    
+    
+    # sample authenticate call    
+    @authenticateResponse = @securityClient.request :authenticate do
+      soap.body = { 
+        :authentication => { 
+          :username => "admin", 
+          :password => "admin" 
+        }
+      }
+    end
+        
   end  
 end
